1cffee2177750fefa6e2ecac0f8906903107a07b
kiaarablu <109739665+kiaarablu@users.noreply.github.com>
2022-09-21 00:15:34 -0700

    Add files via upload

---
+++ ColeringGraph/ColeringGraph/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColeringGraph
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Colering colering = new Colering();
+            colering.Search(7, new int[8]);
+            Console.ReadKey();
+        }
+    }
+}
